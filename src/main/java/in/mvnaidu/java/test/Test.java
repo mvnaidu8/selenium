be51b352db6cd6b782ee36ade3d2f0ae1ab576bb
M Venkat Naidu <info@mvnaidu.in>
2020-02-02 00:31:31 +0530

    Re-direct Sstem out to systemout file

--- src/main/java/in/mvnaidu/java/test/Test.java
+++ src/main/java/in/mvnaidu/java/test/Test.java
@@ -7,6 +7,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+
 import org.openqa.selenium.Alert;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -14,10 +19,20 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class Test {
 	
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
+		
+		
+		File file = new File("systemout");
+        //Instantiating the PrintStream class
+        PrintStream stream = new PrintStream(file);
+        System.out.println("From now on "+file.getAbsolutePath()+" will be your console");
+        System.setOut(stream);
+		
+		
 		driver.get("https://www.testandquiz.com/selenium/testing.html");
+		System.out.println("Current URL = "+ driver.getCurrentUrl());
 		//Text Box
 		driver.findElement(By.id("fname")).sendKeys("Venkat");
 		
@@ -34,6 +49,7 @@ public class Test {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 		//Radio-Button
 		WebElement radioButton = driver.findElement(By.id("male"));
 		radioButton.click();
@@ -60,8 +76,10 @@ public class Test {
 		 //Alert Handling
 		 Alert alert = (Alert) driver.switchTo().alert();
 		 alert.accept();
+		 
+		 System.out.println("Window Handle = "+driver.getWindowHandle());
 				
-				//driver.close();
+				driver.close();
 		System.out.println("Test Ended");
 	}
 
