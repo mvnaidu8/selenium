67b0f9424c0d5379282f9ce85db1ca2a9dce24d7
M Venkat Naidu <info@mvnaidu.in>
2020-04-12 22:29:46 +0530

    SeleniumGrid

---
+++ src/main/java/in/mvnaidu/java/test/SeleniumGrid.java
@@ -0,0 +1,29 @@
+package in.mvnaidu.java.test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class SeleniumGrid {
+
+	public static void main(String[] args) throws MalformedURLException {
+		// TODO Auto-generated method stub
+		DesiredCapabilities dc = new DesiredCapabilities();
+		dc.setBrowserName("chrome");
+		dc.setPlatform(Platform.WINDOWS);
+		
+		
+		WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),dc);
+		
+		driver.get("https://google.com");
+		System.out.println(driver.getTitle());
+		
+		
+		
+	}
+
+}
